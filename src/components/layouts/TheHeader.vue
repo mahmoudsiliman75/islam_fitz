2aa43b767115f763fc828a4ea4e6a45ba94e6593
mahmoudsiliman75 <mahmoudsiliman75@gmail.com>
2021-07-07 04:33:26 -0700

    add sass files

--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -1,11 +1,18 @@
 <template>
   <header>
     <h1>The Header</h1>
+    <Scratch />
   </header>
 </template>
 
 <script>
+import Scratch from "../ui/ScratchShape.vue";
+
 export default {
   name: "Header",
+
+  components: {
+    Scratch,
+  },
 };
 </script>
