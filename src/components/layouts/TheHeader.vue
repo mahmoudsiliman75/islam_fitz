f91dc745bd1d5de5b116e472d55eb84bdfaec8df
mahmoudsiliman75 <mahmoudsiliman75@gmail.com>
2021-08-08 04:11:00 -0700

    finish small screens menu

--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -6,7 +6,12 @@
       </router-link>
     </div>
 
+    <!-- START:: LARGE SCREENS MENU -->
     <div class="menu_wraper">
+      <button class="small_screens_menu_open" @click="openSmallMenu">
+        <i class="fas fa-ellipsis-h fa-2x"></i>
+      </button>
+
       <ul>
         <li class="nav_item">
           <router-link to="/"> الرئيسية </router-link>
@@ -29,11 +34,58 @@
         </li>
       </ul>
     </div>
+    <!-- END:: LARGE SCREENS MENU -->
+
+    <!-- START:: SMALL SCREENS MENU -->
+    <div class="small_screens_menu_wraper" :class="{ open: isOpend }">
+      <button class="small_screens_menu_close" @click="closeSmallMenu">
+        <i class="fas fa-times fa-lg"></i>
+      </button>
+
+      <ul>
+        <li class="nav_item">
+          <router-link to="/"> الرئيسية </router-link>
+        </li>
+
+        <li class="nav_item">
+          <router-link to="/about"> من نحن </router-link>
+        </li>
+
+        <li class="nav_item">
+          <router-link to="/before_and_after"> قبل و بعد </router-link>
+        </li>
+
+        <li class="nav_item">
+          <router-link to="/faqs"> الأسئلة الشائعة </router-link>
+        </li>
+
+        <li class="nav_item">
+          <a href="#"> المدونة </a>
+        </li>
+      </ul>
+    </div>
+    <!-- END:: SMALL SCREENS MENU -->
   </header>
 </template>
 
 <script>
 export default {
   name: "Header",
+
+  data() {
+    return {
+      isOpend: false,
+    };
+  },
+
+  methods: {
+    openSmallMenu() {
+      this.isOpend = true;
+    },
+
+    closeSmallMenu() {
+      this.isOpend = false;
+    },
+  },
 };
 </script>
